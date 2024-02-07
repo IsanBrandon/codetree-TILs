82d12cf06cf04ced9b6f0abdefe1127844b4719d
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-07 11:15:56 +0900

    Title: 좌우로 반복하며 출력, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/좌우로 반복하며 출력/print-in-left-right-reverse.py
+++ 240207/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -2,7 +2,7 @@ n = int(input())
 
 for i in range(n):
     for j in range(n):
-        if i % 2 == 1:
+        if i % 2 == 0:
             print(j + 1, end="")
         else:
             print(n - j, end="")
