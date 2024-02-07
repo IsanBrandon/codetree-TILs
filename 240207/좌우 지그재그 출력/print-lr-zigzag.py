f103fdce593d8a253ed5555b8f591fbd12cfb19f
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-07 11:31:09 +0900

    Title: 좌우 지그재그 출력, Time: 140ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240207/좌우 지그재그 출력/print-lr-zigzag.py
@@ -0,0 +1,14 @@
+n = int(input())
+cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        if i % 2 == 0:
+            cnt += 1
+            print(cnt, end=" ")
+        else:
+            print(cnt, end=" ")
+            cnt -= 1
+        if j == n-1:
+            cnt += n
+    print()
