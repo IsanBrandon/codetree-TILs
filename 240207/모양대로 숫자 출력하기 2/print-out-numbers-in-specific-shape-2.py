dcb77002eccf4eea5d08dbf97517bde471caad0d
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-07 10:58:32 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 162ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.py
+++ 240207/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.py
@@ -1,9 +1,9 @@
 n = int(input())
-cnt = 1
+cnt = 2
 for i in range(n):
     for j in range(n):
-        print(n * cnt)
-        cnt += 1
+        print(cnt, end=" ")
+        cnt += 2
         if cnt == 10:
-            cnt = 1
+            cnt = 2
     print()
