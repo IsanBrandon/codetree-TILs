946be2146f2bd713be7d0ae00916a5cca0794832
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-08 10:35:49 +0900

    Title: 19단 출력, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240208/19단 출력/nineteen-times-table.py
+++ 240208/19단 출력/nineteen-times-table.py
@@ -4,7 +4,7 @@ for i in range(1, 20, 1):
     for j in range(1, 20, 1):
         if j == 19:
             print(f"{i} * {j} = {i*j}")
-        elif: i % 2 == 0
+        elif i % 2 == 0:
             print(f"{i} * {j} = {i*j}")
         else:
             print(f"{i} * {j} = {i*j}", end=" / ")
