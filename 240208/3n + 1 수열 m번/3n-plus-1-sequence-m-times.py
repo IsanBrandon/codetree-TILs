a4115afe35de5d54260738dda71c50a0111acc15
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-08 16:34:09 +0900

    Title: 3n + 1 수열 m번, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 240208/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -3,14 +3,13 @@ m = int(input())
 for _ in range(m):
     n = int(input())
     cnt = 0
-    temp = n
 
-    while temp == 1
-        if temp % 2 == 0:
-            temp //= 2
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
         else:
-            temp *= 3
-            temp += 1
+            n *= 3
+            n += 1
         cnt += 1
 
     print(cnt)
