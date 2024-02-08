5f2407a2727fc0b6268c87296de7ed9b9e0dfc2c
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-08 16:26:00 +0900

    Title: ab곱 n번 구하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240208/ab곱 n번 구하기/a-multiple-b-n-times.py
+++ 240208/ab곱 n번 구하기/a-multiple-b-n-times.py
@@ -3,8 +3,8 @@ n = int(input())
 for _ in range(n):
     arr = input().split()
     a, b = int(arr[0]), int(arr[1])
-    sum_val = 0
+    sum_val = 1
 
     for i in range(a, b+1, 1):
-        sum_val += i
+        sum_val *= i
     print(sum_val)
