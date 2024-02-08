8510f0fa6f363f33316011364d2bf59012c30f24
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-08 10:35:39 +0900

    Title: 19단 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240208/19단 출력/nineteen-times-table.py
@@ -0,0 +1,10 @@
+cnt = 1
+
+for i in range(1, 20, 1):
+    for j in range(1, 20, 1):
+        if j == 19:
+            print(f"{i} * {j} = {i*j}")
+        elif: i % 2 == 0
+            print(f"{i} * {j} = {i*j}")
+        else:
+            print(f"{i} * {j} = {i*j}", end=" / ")
