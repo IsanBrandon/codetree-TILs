b53ca2779c1a09c95024c4bcb964a14f8ba882b2
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-08 16:32:56 +0900

    Title: 3n + 1 수열 m번, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240208/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,16 @@
+m = int(input())
+
+for _ in range(m):
+    n = int(input())
+    cnt = 0
+    temp = n
+
+    while temp == 1
+        if temp % 2 == 0:
+            temp //= 2
+        else:
+            temp *= 3
+            temp += 1
+        cnt += 1
+
+    print(cnt)
