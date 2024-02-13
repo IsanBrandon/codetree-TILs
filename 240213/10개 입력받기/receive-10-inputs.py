cc48e1393791f995e684fc23c62480bffeb052dd
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-13 15:29:59 +0900

    Title: 10개 입력받기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240213/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+cnt = 0
+sum_val = 0
+
+for i in arr:
+    if i == 0:
+        break
+    else:
+        cnt += 1
+        sum_val += i
+
+print(f"{sum_val} {sum_val / cnt :.1f}")
