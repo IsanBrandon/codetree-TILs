8882ac9fe46a768d8bca6b3b773557229706ce41
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-15 15:34:54 +0900

    Title: 숫자들의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240215/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = 0
+arr = [elem * i for i in range(1, 51, 1)]
+
+for elem in arr:
+    if elem % 5 == 0:
+        cnt += 1
+    if cnt == 3:
+        break
+    else:
+        print(elem)
