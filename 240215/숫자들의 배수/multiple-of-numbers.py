8a5eaf51549f177c359faca6a535e95f21a7bd52
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-15 15:37:03 +0900

    Title: 숫자들의 배수, Time: 194ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/숫자들의 배수/multiple-of-numbers.py
+++ 240215/숫자들의 배수/multiple-of-numbers.py
@@ -5,7 +5,6 @@ arr = [n * i for i in range(1, 51, 1)]
 for elem in arr:
     if elem % 5 == 0:
         cnt += 1
-    if cnt == 3:
+    print(elem, end=" ")
+    if cnt == 2:
         break
-    else:
-        print(elem, end=" ")
