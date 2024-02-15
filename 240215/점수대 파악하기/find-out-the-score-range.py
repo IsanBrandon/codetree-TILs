75f856f8805a0e252289071701515a7218966f9a
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-15 17:12:09 +0900

    Title: 점수대 파악하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240215/점수대 파악하기/find-out-the-score-range.py
+++ 240215/점수대 파악하기/find-out-the-score-range.py
@@ -3,7 +3,11 @@ arr = list(map(int, input().split()))
 count_arr = [0] * 11
 
 for elem in arr:
+    if elem == 0:
+        break
+    if elem < 10:
+        continue
     count_arr[elem // 10] += 1
 
-for i in range(11, 1, -1):
-    print(f"{(i - 1) * 10} - {count_arr[i-1]}")
+for i in range(10, 0, -1):
+    print(f"{(i}0 - {count_arr[i]}")
