ec24fc1af7f0aa273ad1e171fa14e80dc7cdb497
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-15 14:54:16 +0900

    Title: 일의 자리 배열, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/일의 자리 배열/array-with-units.py
+++ 240215/일의 자리 배열/array-with-units.py
@@ -1,8 +1,8 @@
 sum_val = 0
-arr = []
+arr = [0, 0]
 arr[0], arr[1] = tuple(map(int, input().split()))
 
-for i in range(3, 11):
+for i in range(2, 10):
     sum_val = arr[i - 1] + arr[i - 2]
     arr.append(sum_val % 10)
 
