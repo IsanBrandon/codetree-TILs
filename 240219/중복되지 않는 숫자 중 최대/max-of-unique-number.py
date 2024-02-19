8763aefa4094bb4b560246cad144dab384643c27
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-19 09:44:57 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240219/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -0,0 +1,16 @@
+n = int(input())
+nums = list(int(input().split()))
+
+max_num = -1
+
+for curr_num in nums:
+    if max_num < curr_num:
+        count = 0 
+        for elem in nums:
+            if elem == curr_num:
+                count += 1
+
+        if count == 1:
+            max_num = curr_num
+
+print(max_num)
