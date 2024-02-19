1d442ff91db80b40c3aa680edb6df8be95d5b00c
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-19 11:16:00 +0900

    Title: 500 근처의 수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240219/500 근처의 수/near-500.py
@@ -0,0 +1,13 @@
+nums = list(map(int, input().split()))
+
+max_num = -1
+min_num = -1
+
+for curr_num in nums:
+    if (curr_num > max_num) and (curr_num > 500):
+        max_num = curr_num
+
+    elif (curr_num < min_num) and (curr_num < 500):
+        min_num = curr_num
+
+print(min_num, max_num)
