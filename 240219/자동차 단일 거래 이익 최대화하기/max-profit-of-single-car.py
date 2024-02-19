33e5b74941246067f5100cf60a4c7ede95071b40
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-19 14:56:54 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240219/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240219/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,27 +1,12 @@
-import sys
-
 n = int(input())
-mobile_prices_per_years = list(map(int, input().split()))
+price = list(map(int, input().split()))
 
-min_price = (2 ** 31) - 1
-when_purchased_index = len(mobile_prices_per_years) - 1
+max_profit = 0
+for i in range(n):
+    for j in range(i+1, n):
+        profit = price[j] - price[i]
 
-for i in range(len(mobile_prices_per_years)):
-    if min_price > mobile_prices_per_years[i]:
-        min_price = mobile_prices_per_years[i]
-        when_purchased_index = i
+        if profit > max_profit:
+            max_profit = profit
 
-if when_purchased_index == len(mobile_prices_per_years) - 1:
-    print("0")
-    sys.exit()
-
-after_purchasing_max_price = 0
-
-for price in mobile_prices_per_years[when_purchased_index + 1:]:
-    if after_purchasing_max_price <= price:
-        after_purchasing_max_price = price
-
-if after_purchasing_max_price - min_price <= 0:
-    print("0")
-else:
-    print(after_purchasing_max_price - min_price)
+print(max_profit)
