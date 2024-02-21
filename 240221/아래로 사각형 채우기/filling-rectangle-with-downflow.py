150fe828dcb3c5bc04c374c489c02d294090426b
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-21 11:35:00 +0900

    Title: 아래로 사각형 채우기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240221/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr_2d = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for i in range(n):
+    num = 1 + i
+    for j in range(n):
+        arr_2d[i][j] += num
+        num += 5
+        print(arr_2d[i][j], end=" ")
+    print()
