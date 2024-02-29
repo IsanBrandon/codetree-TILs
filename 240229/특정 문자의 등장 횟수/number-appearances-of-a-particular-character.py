ba42684332331f8fbf4744301a8d5d1adad1229d
JAEHYUK PARK <hyeok831@gmail.com>
2024-02-29 09:29:30 +0900

    Title: 특정 문자의 등장 횟수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240229/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -0,0 +1,12 @@
+string = input()
+length = len(string)
+cnt_ee = 0
+cnt_eb = 0
+
+for i in range(length-1):
+    if string[i] == 'e' and string[i + 1] == 'e':
+        cnt_ee += 1
+    elif string[i] == 'e' and string[i + 1] == 'b':
+        cnt_eb += 1
+
+print(cnt_ee, cnt_eb)
