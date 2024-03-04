4c13cb58c7513931196a6b139268ec76e502f2ab
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-04 10:19:24 +0900

    Title: 규칙에 따라 밀기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240304/규칙에 따라 밀기/push-by-the-rules.py
+++ 240304/규칙에 따라 밀기/push-by-the-rules.py
@@ -1,20 +1,21 @@
 string = input()
 commend = input()
 
-list_com = list(commend)
 leng_com = len(commend)
+list_str = list(string)
+leng_str = len(string)
 
 for i in range(leng_com):
-    if list_com[i] == 'L':
-        front = list_com[0]
-        for i in range(1, leng_com):
-            list_com[i - 1] = list_com[i]
-        list_com[leng_com - 1] = front
+    if list_str[i] == 'L':
+        front = list_str[0]
+        for i in range(1, leng_str):
+            list_str[i - 1] = list_str[i]
+        list_str[leng_str - 1] = front
 
     else:
-        back = list_com[leng_com - 1]
-        for i in range(leng_com - 1, 0, -1)
-            list_com[i] = list_com[i - 1]
-        list_com[0] = back
+        back = list_str[leng_str - 1]
+        for i in range(leng_str - 1, 0, -1):
+            list_str[i] = list_str[i - 1]
+        list_str[0] = back
 
-print("".join(list_com))
+print("".join(list_str))
