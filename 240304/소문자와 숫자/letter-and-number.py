162727ccd37210c7f2fd3ce9e8ec78c8502cd346
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-04 11:41:11 +0900

    Title: 소문자와 숫자, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240304/소문자와 숫자/letter-and-number.py
@@ -0,0 +1,7 @@
+string = input()
+
+for elem in string:
+    if (elem >= 'A' and elem <= 'Z') or (elem >= 'a' and elem <= 'z'):
+        print(elem.lower(), end="")
+    elif (elem >= '0' and elem <= '9'):
+        print(elem, end="")
