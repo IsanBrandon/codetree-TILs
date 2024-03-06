e45d735fd3c5565e00280aab3008f773bf9070bd
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-06 11:46:54 +0900

    Title: 함수를 이용한 윤년 판별, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240306/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,13 @@
+def special_year(y):
+    if y % 100 == 0 and y % 400 == 0:
+        return False
+    if y % 4 == 0:
+        return True
+    return False
+
+year = int(input())
+
+if special_year(year):
+    print("true")
+else:
+    print("false")
