352cb0fbfb750a47c724cff41d08aa73c608c34d
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-06 10:57:18 +0900

    Title: 함수를 이용한 소수 판별, Time: 143ms, Memory: 73MB, Status: Wrong Answer - CodeTree

---
+++ 240306/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,18 @@
+# a부터 b까지 i를 모두 잡으면서 소수인 것들을 모두 더해준다
+# 소수임을 판정해주는함수를 만든다. 
+
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+a, b = tuple(map(int, input().split()))
+ans = 0
+
+for i in range(a, b+1):
+    if is_prime(i):
+        ans += 1
+
+print(ans)
