bfae1da4a4c3c26858228a7153a2a5a4027c184b
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-07 11:05:45 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240307/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,7 +1,9 @@
+# 변수 선언 및 입력:
 n1, n2 = tuple(map(int, input().split()))
 
-a = list(map(input().split()))
-b = list(map(input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
 
 # n번째 인덱스부터 시작하는 n2길이의 a수열과
 # b수열이 완전히 일치하는지 확인합니다..
@@ -12,13 +14,15 @@ def is_same(n):
 
     return True
 
+
 # b가 a의 연속부분수열인지 확인합니다.
 def is_subsequence():
     for i in range(n1 - n2 + 1):
         if is_same(i):
             return True
-
+    
     return False
+
 
 if is_subsequence():
     print("Yes")
