e6603996a115cd9d5ccf56da1330238a81718be7
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-07 09:53:53 +0900

    Title: 사칙연산 함수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,25 @@
+def plus(n, m):
+    return n + m
+
+def minus(n, m):
+    return n - m
+
+def multiple(n, m):
+    return n * m
+
+def division(n, m):
+    return n // m
+
+a, o, c = tuple(input().split())
+a, c =int(a), int(c)
+
+if o == '+':
+    print(f"{a} * {c} = {plus(a, c)}")
+elif o == '-':
+    print(f"{a} * {c} = {minus(a, c)}")
+elif o == '/':
+    print(f"{a} * {c} = {division(a, c)}")
+elif o == '*':
+    print(f"{a} * {c} = {multiple(a, c)}")
+else:
+    print("False")
