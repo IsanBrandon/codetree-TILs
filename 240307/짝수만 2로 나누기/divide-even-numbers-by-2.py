9a5521194a1b1cea2b1adb9b436d2bdd2f1eaa0f
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-07 15:17:30 +0900

    Title: 짝수만 2로 나누기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ 240307/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -1,16 +1,11 @@
 n = int(input())
-_list = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-def if_even_div_2(arr):
-    leng = len(arr)
-    for i in range(leng):
+def modify(arr):
+    for i in range(n):
         if arr[i] % 2 == 0:
             arr[i] //= 2
-        else:
-            pass
-    return arr
 
-_list = if_even_div_2(_list)
-
-for elem in _list:
+modify(arr)
+for elem in arr:
     print(elem, end=" ")
