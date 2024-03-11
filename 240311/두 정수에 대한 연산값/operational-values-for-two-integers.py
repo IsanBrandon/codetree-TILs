f5796cbf962ba7a32c1d44c6f3628b40caea94ef
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-11 16:53:05 +0900

    Title: 두 정수에 대한 연산값, Time: 235ms, Memory: 24MB, Status: Passed - CodeTree

--- 240311/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240311/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -4,11 +4,11 @@ def modify(a, b):
     if a > b:
         a += 25
         b *= 2
-        return a, b
     else:
         b += 25
         a *= 2
-        return a, b
 
-mod_a, mod_b = modify(a, b)
-print(mod_a, mod_b)
+    return a, b
+
+a, b = modify(a, b)
+print(a, b)
