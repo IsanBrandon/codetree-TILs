188bbb2ef6dccda42ba22c1cd1dacd69c4e22404
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-11 17:14:20 +0900

    Title: 2개 이상의 알파벳, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240311/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240311/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -3,11 +3,11 @@ leng = len(string)
 
 def is_magic_string(string):
     s = string[0]
-    for i in range(1, leng+1):
-        if string[i] == s:
-            return False
-        else:
-            return True
+    flag = False
+    for i in range(1, leng):
+        if string[i] != s:
+            flag = True
+    return flag
 
 if is_magic_string(string):
     print("Yes")
