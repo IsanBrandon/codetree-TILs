a979ac6982395a34356c9503566caa83afff3022
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-18 15:18:28 +0900

    Title: 특정 구간의 합, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240318/특정 구간의 합/sum-of-specific-intervals.py
+++ 240318/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,12 +1,12 @@
 n, m = tuple(map(int, input().split()))
-arr = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
-def sum_val(a, b):
-    global arr 
-    sumsumsum = 0
-    for i in range(a-1, b):
-        sumsumsum += arr[i]
-    return sumsumsum
+def sum_val(a, b): 
+    return_value = 0
+    for i in range(a, b+1):
+        return_value += arr[i]
+
+    return return_value
 
 
 for _ in range(m):
