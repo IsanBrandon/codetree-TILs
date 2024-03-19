e266a7c3fa40994fa1fba1b4ae82c3bf823630c7
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-19 10:02:01 +0900

    Title: 재귀함수의 꽃, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240319/재귀함수의 꽃/the-flower-of-the-recursive-function.py
+++ 240319/재귀함수의 꽃/the-flower-of-the-recursive-function.py
@@ -1,19 +1,12 @@
 n = int(input())
 
-def down_nums(n):
+def print_nums(n):
     if n == 0:
         return
 
     print(n, end=" ")
-    down_nums(n-1)
-
-
-def up_nums(n):
-    if n == 0:
-        return
-
-    up_nums(n-1)
+    print_nums(n-1)
     print(n, end=" ")
 
-down_nums(n)
-up_nums(n)
+
+print_nums(n)
