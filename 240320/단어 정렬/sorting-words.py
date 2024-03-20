c4b2ba1afda837a4c8eb395e1c5e3d63fc1b5080
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-20 11:19:18 +0900

    Title: 단어 정렬, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240320/단어 정렬/sorting-words.py
+++ 240320/단어 정렬/sorting-words.py
@@ -1,9 +1,21 @@
+# MINE
 n = int(input())
-arr = []
+# arr = []
 
-for _ in range(n):
-    arr.append(input())
+# for _ in range(n):
+#     arr.append(input())
 
-arr = sorted(arr)
-for elem in arr:
-    print(elem)
+# arr = sorted(arr)
+# for elem in arr:
+#     print(elem)
+
+# CODETREE
+word_list = [
+    input()
+    for _ in range(n)
+]
+
+word_list.sort()
+
+for word in word_list:
+    print(word)
