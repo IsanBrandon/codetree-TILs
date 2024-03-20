0bfee3c6e84ad6ea31ecb1494d0e377f216cff23
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-20 09:42:16 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240320/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
+++ 240320/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-def get_ans(n):
-    if n == 1:
+def get_ans(a):
+    if a == 1:
         return 0
     
-    if n % 2 == 0:
-        return get_ans(n // 2) + 1
+    if a % 2 == 0:
+        return get_ans(a // 2) + 1
     else:
-        return get_ans(3 * n + 1) + 1
+        return get_ans(3 * a + 1) + 1
 
-print(get_ans(n))
+print(get_ans(a))
