44e003493690a4526ec1e5c961d27febe91e391b
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-20 11:32:56 +0900

    Title: 두 개의 동일한 수열, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240320/두 개의 동일한 수열/two-equal-series.py
+++ 240320/두 개의 동일한 수열/two-equal-series.py
@@ -7,6 +7,7 @@ def flag():
     # 전부 동일한 경우에만 일치합니다.
     # 단 하나라도 다르다면, false입니다.
     for elem1, elem2 in zip(arrA, arrB):
+        # 두 리스트를 한번에 받아오고 싶을 때 zip을 활용해보자.
         if elem1 != elem2:
             return False
 
@@ -22,6 +23,7 @@ arrB.sort()
 
 # 수열이 일치하는지 확인.
 if flag():
+    # if문에 이렇게 함수를 박아둬도 된다.
     print("Yes")
 else:
     print("No")
