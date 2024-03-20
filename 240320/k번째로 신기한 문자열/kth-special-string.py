bc4d6a13a02b3d6724b367b8988b8ddc8ce00754
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-20 17:34:01 +0900

    Title: k번째로 신기한 문자열, Time: 158ms, Memory: 25MB, Status: Passed - CodeTree

--- 240320/k번째로 신기한 문자열/kth-special-string.py
+++ 240320/k번째로 신기한 문자열/kth-special-string.py
@@ -1,23 +1,43 @@
-n, k, T = input().split()
+n, k, t = input().split()
 n, k = int(n), int(k)
-len_T = len(T)
+# len_T = len(T)
 
-arr = [
-    input()
-    for _ in range(n)
-]
+# arr = [
+#     input()
+#     for _ in range(n)
+# ]
 
-arr.sort()
+# arr.sort()
 
-ans_arr = []
+# ans_arr = []
 
-for elem in arr:
-    if elem[0] == T[0]:
-        flag = True
-        for i in range(1, len_T):
-            if elem[i] != T[i]:
-                flag = False
-    if flag:
-        ans_arr.append(elem)
+# for elem in arr:
+#     if elem[0] == T[0]:
+#         flag = True
+#         for i in range(1, len_T):
+#             if elem[i] != T[i]:
+#                 flag = False
+#     if flag:
+#         ans_arr.append(elem)
 
-print(ans_arr[k-1])
+# print(ans_arr[k-1])
+
+# a가 b로 시작하는지를 확인.
+def starts_with(a, b):
+    # b의 길이가 더 길 수는 없음.
+    if len(a) < len(b):
+        return False
+
+    # b의 길이만큼 살펴보며, a와 문자열이 완벽히 동일한지 확인.
+    return a[:len(b)] == b
+
+words = []
+for _ in range(n):
+    word = input()
+    if starts_with(word, t):
+        words.append(word)
+
+# 정렬을 진행. 
+words.sort()
+
+print(words[k - 1])
