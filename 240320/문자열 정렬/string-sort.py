093e04dc61ee85f12342de41659f95196d411226
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-20 11:05:33 +0900

    Title: 문자열 정렬, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240320/문자열 정렬/string-sort.py
@@ -0,0 +1,12 @@
+# arr = list(string)
+# arr.sort()
+# sorted_str = ''.join(arr)
+
+# string = "apple"
+# sorted_arr = sorted(string)
+# sorted_str = ''.join(sorted_arr)
+
+string = input()
+sorted_arr = sorted(string)
+sorted_str = ''.join(sorted_arr)
+print(sorted_str)
