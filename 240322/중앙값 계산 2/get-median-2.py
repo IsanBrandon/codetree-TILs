92bcd0c66746678a0de321938e5a36b6ce521e84
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-22 10:09:14 +0900

    Title: 중앙값 계산 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240322/중앙값 계산 2/get-median-2.py
+++ 240322/중앙값 계산 2/get-median-2.py
@@ -1,11 +1,21 @@
 n = int(input())
 arr = list(map(int, input().split()))
-arr.sort()
 
+# 1. 입력 받은 번째까지 새로운 리스트에 담아내고
+# 2. sorting 시킨 다음
+# 3. 중간값 뽑아주기
+def sorting(n):
+    arr_sorted = []
+    for j in range(n):
+        arr_sorted.append(arr[j])
+    arr_sorted.sort()
+    return arr_sorted[((n-1) // 2)]
 
-
-
+ans_arr = []
 
 for i in range(n):
     if i % 2 == 0:
-        print(arr[i//2], end=" ")
+        ans_arr.append(sorting(i))
+
+for elem in ans_arr:
+    print(elem, end=" ")
