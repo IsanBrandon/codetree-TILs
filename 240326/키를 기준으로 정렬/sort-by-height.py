f6f5b3286db82c10bd986db5e07fc24716944059
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-26 15:25:36 +0900

    Title: 키를 기준으로 정렬, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/키를 기준으로 정렬/sort-by-height.py
+++ 240326/키를 기준으로 정렬/sort-by-height.py
@@ -3,8 +3,12 @@ class People:
         self.name, self.height, self.weight = name, height, weight
 
 n = int(input())
-arr = [tuple(input().split()) for _ in range(n)]
-people = [People(name, height, weight) for name, height, weight in arr]
+# arr = [tuple(input().split()) for _ in range(n)]
+# people = [People(name, height, weight) for name, height, weight in arr]
+people = []
+for _ in range(n):
+    name, height, weight = tuple(input().split())
+    people.append(People(name, int(height), int(weight)))
 
 people.sort(key=lambda x: x.height)
 
