f9d4b0b6dc2b09d85dcad468b899dd1f01103152
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-26 10:41:24 +0900

    Title: 코드네임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240326/코드네임/code-name.py
+++ 240326/코드네임/code-name.py
@@ -1,17 +1,21 @@
+# 클래스 선언
 class Agents:
-    def __init__(self, name, score):
-        self.name = name
+    def __init__(self, code_name, score):
+        self.code_name = code_name
         self.score = score
 
-agents = [
-    tuple(map(int, input().split()))
-    for _ in range(5)
-]
+# 변수 선언 및 입력
+agents = []
 
-agent1 = Agents[0]
-agent2 = Agents[1]
-agent3 = Agents[2]
-agent4 = Agents[3]
-agent5 = Agents[4]
+for _ in range(5):
+    code_name, score = tuple(input().split())
+    agents.append[Agents(code_name, int(score))]
 
-for i in range(5):
+# 최소 점수를 갖는 유저 찾기
+min_idx = 0 
+for i in range(1, 5):
+    if agents[min_idx].score > agents[i].score:
+        min_idx = i
+
+# 출력
+print(agents[min_idx].code_name, agents[min_idx].score)
