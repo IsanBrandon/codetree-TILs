a2038be4d492b8bb5d036afc0a69810ab2a7587e
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-27 16:43:11 +0900

    Title: 그 요일은, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,19 @@
+MONTHS = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+WEEKEND = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+m1, d1, m2, d2 = map(int, input().split())
+week = input()
+
+days = MONTHS[m1] - d1 + 1 + sum(MONTHS[m1+1:m2]) + d2 if m1 != m2 else d2 - d1 + 1
+# print(days)
+# print(days//7, days%7)
+
+
+if WEEKEND.index(week)+1 <= (days % 7) and days > 7:
+    # print("y")
+    print(days//7 + 1)
+elif days<7:
+  if WEEKEND.index(week)+1 <= (days % 7):
+    print(1)
+else:
+    print(days//7)
