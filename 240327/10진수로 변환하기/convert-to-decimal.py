11f69cb0cebeabb0f7d63aff0ffe09d5f531c5b4
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-27 17:36:09 +0900

    Title: 10진수로 변환하기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/10진수로 변환하기/convert-to-decimal.py
+++ 240327/10진수로 변환하기/convert-to-decimal.py
@@ -1,14 +1,23 @@
-bi = input()
+binary = list(map(int, list(input())))
+length = len(binary)
+
 num = 0
-
-binary = []
-
-for elem in bi:
-    binary.append(int(elem))
-
-len_bi = len(binary)
-
-for i in range(len_bi):
+for i in range(length):
     num = num * 2 + binary[i]
 
 print(num)
+
+### MINE ###
+# bi = input()
+# binary = []
+
+# for elem in bi:
+#     binary.append(int(elem))
+
+# len_bi = len(binary)
+
+# num = 0
+# for i in range(len_bi):
+#     num = num * 2 + binary[i]
+
+# print(num)
