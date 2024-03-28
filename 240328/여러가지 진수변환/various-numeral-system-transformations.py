3ed8bdabc6b3e3865c22bc08ea459c1bb7122f30
JAEHYUK PARK <hyeok831@gmail.com>
2024-03-28 09:26:23 +0900

    Title: 여러가지 진수변환, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240328/여러가지 진수변환/various-numeral-system-transformations.py
+++ 240328/여러가지 진수변환/various-numeral-system-transformations.py
@@ -2,12 +2,12 @@ n, b = tuple(map(int, input().split()))
 digits = []
 
 while True:
-    if n < 4:
+    if n < b:
         digits.append(n)
         break
 
-    digits.append(n % 4)
-    n //= 4
+    digits.append(n % b)
+    n //= b
 
 for digit in digits[::-1]:
     print(digit, end="")
