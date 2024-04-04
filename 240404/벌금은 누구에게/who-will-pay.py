4ea6ed2d43d559031bc20fa41deb5fe4b4f38cf0
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-04 16:05:15 +0900

    Title: 벌금은 누구에게, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240404/벌금은 누구에게/who-will-pay.py
+++ 240404/벌금은 누구에게/who-will-pay.py
@@ -1,12 +1,31 @@
+# 변수 선언 및 입력
 n, m, k = tuple(map(int, input().split()))
+penalized_person = [
+    int(input())
+    for _ in range(m)
+]
+penalty_num = [0] * (n + 1)
 
-arr = [k] * (n + 1)
+# 각 패널티 횟수를 세서,
+# 최초로 k번 이상 벌칙을 받는 사람을 추적합니다.
+ans = -1
+for target in penalized_person:
+    penalty_num[target] += 1
 
-ans = 0
-for _ in range(m):
-    minus_num = int(input())
-    arr[minus_num] -= 1
-    if arr[minus_num] <= 0:
-        ans = minus_num
+    if penalty_num[target] >= k:
+        ans = target
         break
+
 print(ans)
+
+# arr = [k] * (n + 1)
+
+# ans = 0
+# for _ in range(m):
+#     minus_num = int(input())
+#     arr[minus_num] -= 1
+#     if arr[minus_num] <= 0:
+#         ans = minus_num
+#         break
+
+# print(ans)
