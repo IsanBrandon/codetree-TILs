75beaeda5c2b741277e634ad39266063f6516863
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-04 15:52:29 +0900

    Title: 벌금은 누구에게, Time: 195ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240404/벌금은 누구에게/who-will-pay.py
+++ 240404/벌금은 누구에게/who-will-pay.py
@@ -6,7 +6,7 @@ ans = 0
 for _ in range(m):
     minus_num = int(input())
     arr[minus_num] -= 1
-    if arr[minus_num] == 0:
+    if arr[minus_num] <= 0:
         ans = minus_num
         break
 print(ans)
