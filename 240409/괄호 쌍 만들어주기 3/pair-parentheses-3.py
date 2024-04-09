0e48f967ee5394e451e6ff4ead47441d7d59184e
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-09 15:52:25 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240409/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 240409/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -1,26 +1,22 @@
+# 변수 선언 및 입력
 string = input()
 leng = len(string)
 
+# 모든 쌍을 다 잡아봅니다.
 cnt = 0
 for i in range(leng):
-    if string[i] == '(':
-        for j in range(i + 1, leng):
-            if string[j] == ')':
-                cnt += 1
+    for j in range(i+1, leng):
+        if string[i] == '(' and string[j] == ')':
+            cnt += 1
 
 print(cnt)
 
 
-# max_sum = 0 
-# for i in range(n):
-#     for j in range(i + 1, n):
-#         arr[i], arr[j] = arr[i] * 2, arr[i] * 2
+# cnt = 0
+# for i in range(leng):
+#     if string[i] == '(':
+#         for j in range(i + 1, leng):
+#             if string[j] == ')':
+#                 cnt += 1
 
-#         sum_diff = 0
-#         for k in range(n - 1):
-#             sum_diff += abs(arr[k + 1] - arr[k])
-
-#         max_sum = max(max_sum, sum_diff)
-#         arr[i], arr[j] = arr[i] // 2, arr[j] // 2
-
-# print(max_sum)
+# print(cnt)
