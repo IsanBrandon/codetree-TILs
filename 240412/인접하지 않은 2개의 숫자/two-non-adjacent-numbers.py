07416f1b2f08cff81349111e5698c4aa42eae10f
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-12 10:13:43 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 240412/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240412/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -1,14 +1,23 @@
 import sys
+
 INT_MIN = -sys.maxsize
 
+# 변수 선언 및 입력
 n = int(input())
 arr = list(map(int, input().split()))
 
+# 인접하지 않은 모든 쌍을 다 잡아봅니다. 
 ans = INT_MIN
-sum_val = 0
 for i in range(n):
-    for j in range(i+2, n):
-        sum_val = arr[i] + arr[j]
-        ans = max(ans, sum_val)
+    for j in range(i + 2, n):
+        if ans < arr[i] + arr[j]:
+            ans = arr[i] + arr[j]
+
+###### MINE ######
+# sum_val = 0
+# for i in range(n):
+#     for j in range(i+2, n):
+#         sum_val = arr[i] + arr[j]
+#         ans = max(ans, sum_val)
 
 print(ans)
