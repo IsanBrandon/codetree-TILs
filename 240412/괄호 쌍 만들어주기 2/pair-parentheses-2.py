037717f05dea025471a22659f90938f8bc82e61b
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-12 09:50:46 +0900

    Title: 괄호 쌍 만들어주기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240412/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,11 @@
+string = input()
+n = len(string)
+
+cnt = 0
+for i in range(n):
+    if string[i] == '(' and string[i + 1] == '(':
+        for j in range(i + 2, n - 1):
+            if string[j] == ')' and string[j + 1] == ')':
+                cnt += 1
+
+print(cnt)
