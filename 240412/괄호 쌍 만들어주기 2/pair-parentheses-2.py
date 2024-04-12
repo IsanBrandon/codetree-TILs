3ab68bf8d39d22d348f6bf380548ced497a73608
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-12 09:55:28 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240412/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240412/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -3,7 +3,7 @@ n = len(string)
 
 cnt = 0
 for i in range(n - 1):
-        for j in range(i + 1, n - 1):
+        for j in range(i + 2, n - 1):
             if string[i] == '(' and string[i + 1] == '(' and string[j] == ')' and string[j + 1] == ')':
                 cnt += 1
 
