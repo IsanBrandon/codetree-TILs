a19e17663e8f461aaf1e0debc8399a23cc5778d9
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-15 10:58:51 +0900

    Title: 구간 중 최대 합, Time: 236ms, Memory: 24MB, Status: Passed - CodeTree

--- 240415/구간 중 최대 합/max-sum-of-subarray.py
+++ 240415/구간 중 최대 합/max-sum-of-subarray.py
@@ -1,15 +1,25 @@
-import sys
+# import sys
 
-INT_MIN = -sys.maxsize
+# INT_MIN = -sys.maxsize
 
+# 변수 선언 및 입력
 n, k = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-ans = INT_MIN
-for i in range(n - k + 1):
-    max_val = 0
+# 모든 구간의 시작점을 잡아봅니다.
+# ans = INT_MIN
+max_sum = 0
+for i in range(0, n - k + 1):
+    # 해당 구간 내 원소의 합을 구합니다.
+    # max_val = 0
+    interval_sum = 0 
     for j in range(i, i + k):
-        max_val += arr[j]
-    ans = max(ans, max_val)
+    #    max_val += arr[j]
+        interval_sum += arr[j]
 
-print(ans)
+    # 최댓값을 구합니다.
+    # ans = max(ans, max_val)
+    max_sum = max(max_sum, interval_sum)
+
+# print(ans)
+print(max_sum)
