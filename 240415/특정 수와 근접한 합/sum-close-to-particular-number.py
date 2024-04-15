732c499d0cb1941c180f7e653fec4012d528a388
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-15 10:33:23 +0900

    Title: 특정 수와 근접한 합, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240415/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240415/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,6 +1,8 @@
 import sys
+
 INT_MAX = sys.maxsize
 
+# 변수 선언 및 입력:
 n, s = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
@@ -10,8 +12,8 @@ for i in range(n):
     for j in range(n):
         if i == j:
             continue
-        a = sum_val - (arr[i] + arr[j])
-        gap = abs(s - a)
+        new_sum = sum_val - (arr[i] + arr[j])
+        gap = abs(s - new_sum)
         min_gap = min(min_gap, gap)
 
 print(min_gap)
