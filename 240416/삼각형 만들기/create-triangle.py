4d69af9226dc71c396bd0d2fbd8502fd63768f8e
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-16 14:06:52 +0900

    Title: 삼각형 만들기, Time: 206ms, Memory: 26MB, Status: Passed - CodeTree

--- 240416/삼각형 만들기/create-triangle.py
+++ 240416/삼각형 만들기/create-triangle.py
@@ -7,8 +7,8 @@ points = [
 
 # 삼각형의 넓이에 2를 곱한 값을 반환합니다.
 def area(x1, y1, x2, y2, x3, y3):
-    return abs((x1 * y2 + x2 * y3 + x3 * y1) - 
-               (x2 * y1 + x3 * y2 + x1 * y3))
+    return abs((x1 * y2 + x2 * y3 + x3 * y1) - \
+                (x2 * y1 + x3 * y2 + x1 * y3))
 
 
 # 3개의 점을 모두 골라보면서
@@ -26,8 +26,8 @@ for i in range(n):
             x3, y3 = points[k]
             if (x1 == x2 or x1 == x3 or x2 == x3) and \
                 (y1 == y2 or y1 == y3 or y2 == y3):
-                max_area = max(max_area,
-                               area(x1, y1, x2, y2, x3, y3))
+                max_area = max(max_area, \
+                                area(x1, y1, x2, y2, x3, y3))
 
 print(max_area)
 
