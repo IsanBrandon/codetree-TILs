765f169b00bea38113650b41bc78f57dad736af7
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-16 11:15:24 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240416/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -2,36 +2,40 @@ import sys
 
 INT_MAX = sys.maxsize
 
+# 변수 선언 및 입력
 n = int(input())
 points = [
     tuple(map(int, input().split()))
     for _ in range(n)
 ]
 
-ans = INT_MAX
+
+# 두 점 사이의 거리의 제곱 값을 반환합니다.
+def dist(i, j):
+    x1, y1 = points[i]
+    x2, y2 = points[j]
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+
+
+# 모든 쌍에 대해서 거리 제곱값을 계산하여
+# 그 중 최솟값을 찾습니다.
+min_dist = INT_MAX
 for i in range(n):
     for j in range(i + 1, n):
-        distance = 0
+        min_dist = min(min_dist, dist(i, j))
 
-        x1, y1 = points[i]
-        x2, y2 = points[j]
-        distance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
-        ans = min(ans, distance)
-
-print(ans)
+print(min_dist)
 
 
-
+###### MINE ######
+# ans = INT_MAX
 # for i in range(n):
 #     for j in range(i + 1, n):
-#         count = [0] * 11 # initialize
-#         for k in range(n):
-#             if k == i or k == j:
-#                 continue
+#         distance = 0
 
-#             x1, x2 = segments[k]
-#             for l in range(x1, x2 + 1):
-#                 count[l] += 1
+#         x1, y1 = points[i]
+#         x2, y2 = points[j]
+#         distance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+#         ans = min(ans, distance)
 
-#         max_cnt = max(count)
-#         ans = min(ans, max_cnt)
+# print(ans)
