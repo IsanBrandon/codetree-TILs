d2127a98fbda1fdf0599038ff32799f00250141d
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-16 10:24:14 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240416/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 240416/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -3,26 +3,40 @@ import sys
 INT_MAX = sys.maxsize
 INT_MIN = -sys.maxsize
 
+# 변수 선언 및 입력
 n = int(input())
-
 arr = [
     tuple(map(int, input().split()))
     for _ in range(n)
 ]
 
 ans = INT_MAX
+
+# 빼야하는 점의 위치를 정합니다.
 for i in range(n):
-    x_min, x_max, y_min, y_max = INT_MAX, INT_MIN, INT_MAX, INT_MIN
+    # i번 점을 제외한 나머지 점들을 포함하기 위한
+    # 직사각형의 최소 넓이를 구합니다.
+
+    # 직사각형의 최소 넓이를 구하기 위해서는,
+    # 남은 점들의 x좌표 중 최소, 최대
+    #            y좌표 중 최소, 최대를 구해야 합니다.
+    min_x, max_x = INT_MAX, 1 
+    min_y, max_y = INT_MAX, 1
     squre = 0
-    for j in range(n):
+    for j, (x, y) in enumerate(arr):
+        # i번째 점은 제외합니다.
         if j == i:
             continue
 
-        x, y = arr[j]
-        x_min, x_max = min(x_min, x), max(x_max, x)
-        y_min, y_max = min(y_min, y), max(y_max, y)
+    # for j in range(n):
+    #     if j == i:
+    #         continue
+
+    #     x, y = arr[j]
+        min_x, max_x = min(min_x, x), max(max_x, x)
+        min_y, max_y = min(min_y, y), max(max_y, y)
     
-    squre = (x_max - x_min) * (y_max - y_min)
+    squre = (max_x - min_x) * (max_y - min_y)
     ans = min(squre, ans)
 
 print(ans)
