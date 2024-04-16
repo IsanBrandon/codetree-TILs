700f1f063a2197083ce4c4715b785fc6b234044f
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-16 16:15:50 +0900

    Title: 스승의 은혜 2, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240416/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,26 @@
+n, b = tuple(map(int, input().split()))
+arr = [
+    int(input())
+    for _ in range(n)
+]
+
+sorted_arr = sorted(arr)
+
+curr_exp = 0
+cnt = 0
+feasibility = 0
+for i in range(n):
+    curr_exp += arr[i]
+    cnt += 1
+
+    if curr_exp >= b:
+        curr_exp -= arr[i]
+        cnt -= 1
+        feasibility = arr[i] // 2
+        if curr_exp + feasibility >= b:
+            print(cnt)
+            break
+        else:
+            cnt += 1
+            print(cnt)
+            break
