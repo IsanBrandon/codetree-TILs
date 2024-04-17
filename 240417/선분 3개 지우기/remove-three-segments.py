1c381b24ca54a011046d8975be4bf2de9dc3572f
JAEHYUK PARK <hyeok831@gmail.com>
2024-04-17 16:49:28 +0900

    Title: 선분 3개 지우기, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240417/선분 3개 지우기/remove-three-segments.py
@@ -0,0 +1,55 @@
+MAX_A = 100
+
+# 변수 선언 및 입력 
+n = int(input())
+section = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+
+# def get_max_overlapped_cnt(i1, i2, i3):
+#     count = [0] * 101
+#     for i in range(n):
+#         if i in [i1, i2, i3]:
+#             continue
+        
+#         x1, x2 = section[i]
+#         for j in range(x1, x2 + 1):
+#             count[j] += 1
+
+#         return max(count)
+
+
+# 3개의 선분을 모두 골라보면서
+# 모두 겹쳐지지 않도록 하는 가짓수를 구합니다.
+ans = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            # i, j, k번 선분을 제외했을 때
+            # 모든 선분이 겹치지 않으면 정답을 1 추가합니다. 
+
+            # overlap : 모든 선분이 겹치지 않으면 false
+
+            # if get_max_overlapped_cnt(i, j, k) == 1:
+            #     ans += 1
+            overlap = False 
+            arr = [0] * (MAX_A + 1)
+
+            for x in range(n):
+                # 제외한 3개의 선분이면 넘어갑니다.
+                if x == i or x == j or x == k:
+                    continue
+
+                for y in range(section[x][0], section[x][1] + 1):
+                    arr[y] += 1
+
+            for x in range(MAX_A + 1):
+                if arr[x] > 1:
+                    overlap = True 
+
+            if overlap == False:
+                ans += 1
+
+print(ans)
