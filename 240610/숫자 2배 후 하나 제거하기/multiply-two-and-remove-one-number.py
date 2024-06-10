f282b382420ec4e3db293ab750ba3373b7cf1c1f
JAEHYUK PARK <hyeok831@gmail.com>
2024-06-10 11:46:16 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240610/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
+++ 240610/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -5,41 +5,66 @@ INT_MAX = sys.maxsize
 n = int(input())
 arr = list(map(int, input().split()))
 
+# 제거된 원소의 위치를 removed_idx라 가정했을 때,
+# 인접한 원소간의 차의 합을 계산해 반환합니다. 
+def score(removed_idx):
+    # 합을 계산합니다
+    sum_val = 0
+
+    # 바로 직전에 젹혀있던 숫자를 기록합니다.
+    # -1인 경우, 아직 숫자가 기록된 적이 없다는 뜻입니다. 
+    prev = -1 
+
+    # 각 숫자들을 순회합니다.
+    for i in range(n):
+        # 지워진 원소라면 패스합니다. 
+        if i == removed_idx:
+            continue 
+        
+        # 이전에 적혀있던 숫자가 있는 경우에만
+        # 인접한 숫자간의 차를 sum_val에 더해줍니다. 
+        if prev != -1:
+            sum_val += abs(arr[i] - prev)
+        
+        # 마지막으로 적혀있던 숫자 값을 갱신해줍니다.
+        prev = arr[i] 
+
+    # 인접한 원소간의 차의 합을 반환합니다. 
+    return sum_val 
+
 min_diff = INT_MAX 
+
+# 2배를 할 숫자의 위치 i를 결정하고,
+# 그 다음 제거할 숫자의 위치 j를 결정하여
+# 남은 숫자들의 인접한 차의 합 중
+# 최솟값을 찾습니다. 
 for i in range(n):
-    arr [i] *= 2
+    arr[i] *= 2
 
+    # 제거할 숫자의 위치를 결정합니다.
     for j in range(n):
-        remaining_arr = [elem for k, elem in enumerate(arr) if k != j]
-        
-        sum_diff = 0 
-        for k in range(n - 2):
-            sum_diff += abs(remaining_arr[k + 1] - remaining_arr[k])
-        
-        min_diff = min(min_diff, sum_diff) 
+        min_score = min(min_score, score(j))
 
+    # 그 다음 상태 진행을 위해,
+    # 이전에 2배를 한 값을 다시 이전 상태로 만들어줍니다. 
     arr[i] //= 2
 
-print(min_diff) 
-        
+print(min_score)
 
-# n = 5
-# arr = [1, 5, 2, 6, 8]
 
-# max_diff = 0 
+
 # for i in range(n):
-#     arr[i] *= 2
+#     arr [i] *= 2
 
 #     for j in range(n):
-#         # (METHOD3)
 #         remaining_arr = [elem for k, elem in enumerate(arr) if k != j]
-
+        
 #         sum_diff = 0 
 #         for k in range(n - 2):
 #             sum_diff += abs(remaining_arr[k + 1] - remaining_arr[k])
-
-#         max_diff = max(max_diff, sum_diff)
+        
+#         min_diff = min(min_diff, sum_diff) 
 
 #     arr[i] //= 2
 
-# print(max_diff)
+# print(min_diff)
