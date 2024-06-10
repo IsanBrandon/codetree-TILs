712ca0e50a6328b23a4cfc2b7e7ec4d73145e1ef
JAEHYUK PARK <hyeok831@gmail.com>
2024-06-10 11:34:15 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 192ms, Memory: 25MB, Status: Passed - CodeTree

--- 240610/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
+++ 240610/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -1,31 +1,45 @@
-n = 5
-arr = [1, 5, 2, 6, 8]
+import sys
 
-max_diff = 0 
+INT_MAX = sys.maxsize
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+min_diff = INT_MAX 
 for i in range(n):
-    arr[i] *= 2
+    arr [i] *= 2
 
     for j in range(n):
-        remaining_arr = []
-        # (METHOD1)
-        # for k in range(n):
-        #     if k != j:
-        #         remaining_arr.append(arr[k])
-        
-        # (METHOD2)
-        # for k, elem in enumerate(arr):
-        #     if k != j:
-        #         remaining_arr.append(elem)
-
-        # (METHOD3)
         remaining_arr = [elem for k, elem in enumerate(arr) if k != j]
-
+        
         sum_diff = 0 
         for k in range(n - 2):
             sum_diff += abs(remaining_arr[k + 1] - remaining_arr[k])
-
-        max_diff = max(max_diff, sum_diff)
+        
+        min_diff = min(min_diff, sum_diff) 
 
     arr[i] //= 2
 
-print(max_diff)
+print(min_diff) 
+        
+
+# n = 5
+# arr = [1, 5, 2, 6, 8]
+
+# max_diff = 0 
+# for i in range(n):
+#     arr[i] *= 2
+
+#     for j in range(n):
+#         # (METHOD3)
+#         remaining_arr = [elem for k, elem in enumerate(arr) if k != j]
+
+#         sum_diff = 0 
+#         for k in range(n - 2):
+#             sum_diff += abs(remaining_arr[k + 1] - remaining_arr[k])
+
+#         max_diff = max(max_diff, sum_diff)
+
+#     arr[i] //= 2
+
+# print(max_diff)
