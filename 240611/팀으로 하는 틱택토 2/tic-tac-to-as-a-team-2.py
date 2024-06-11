92a8ed3589cd043c2dff8e66fe6283aa30636a6f
JAEHYUK PARK <hyeok831@gmail.com>
2024-06-11 10:23:56 +0900

    Title: 팀으로 하는 틱택토 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240611/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240611/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -6,20 +6,20 @@ board = [
     for _ in range(MAX_A)
 ]
 
-ans = 0 
+ans = 0
 
 # 모든 쌍에 대해 전부 시도해 봅니다.
 for i in range(1, MAX_X + 1):
     for j in range(i + 1, MAX_X + 1):
         # i, j 두 명이 팀이 됐을 때 이길 수 있는지를 확인합니다.
-        # win : 두 명이 팀이 됐을 때 이길 수 있다면 true 
-        win = False 
+        # win : 두 명이 팀이 됐을 때 이길 수 있다면 true
+        win = False
 
-        # num_i, num_j : 각 줄에 i, j가 나오는 개수
+        # num_i , num_j : 각 줄에 i, j가 나오는 개수
         num_i = 0
-        num_j = 0 
+        num_j = 0
 
-        # 가로로 빙고가 만들어지 때
+        # 가로로 빙고가 만들어질 때
         for k in range(MAX_A):
             num_i = 0
             num_j = 0
@@ -30,9 +30,9 @@ for i in range(1, MAX_X + 1):
                     num_j += 1
 
             # 3개의 칸에 i, j가 총 3번 나오며
-            # 둘 다 최소 1번 이상 나오면 승리 
+            # 둘 다 최소 1번 이상 나오면 승리
             if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
-                win = True 
+                win = True
 
         # 세로로 빙고가 만들어질 때
         for k in range(MAX_A):
@@ -44,34 +44,40 @@ for i in range(1, MAX_X + 1):
                 if board[l][k] == j:
                     num_j += 1
 
-        if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
-            win = True
+            # 3개의 칸에 i, j가 총 3번 나오며
+            # 둘 다 최소 1번 이상 나오면 승리
+            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
 
         # 왼쪽 위에서 오른쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
         num_i = 0
-        num_j = 0 
+        num_j = 0
         for k in range(MAX_A):
             if board[k][k] == i:
                 num_i += 1
             if board[k][k] == j:
                 num_j += 1
 
+        # 3개의 칸에 i, j가 총 3번 나오며
+        # 둘 다 최소 1번 이상 나오면 승리
         if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
             win = True
 
-        # 오른쪽 위에서 왼쪽 아래를 잇는 대각선으로 빙고가 만들어질 때 
+
+        # 오른쪽 위에서 왼쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
         num_i = 0
         num_j = 0
         for k in range(MAX_A):
             if board[k][MAX_A - k - 1] == i:
-                num_i += 1 
+                num_i += 1
             if board[k][MAX_A - k - 1] == j:
-                num_j += 1 
+                num_j += 1
 
         # 3개의 칸에 i, j가 총 3번 나오며
         # 둘 다 최소 1번 이상 나오면 승리
         if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
             win = True
+
 
         if win:
             ans += 1
