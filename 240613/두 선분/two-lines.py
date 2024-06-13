858fc517975ce1f103725ad3a0c86be654fde0b4
JAEHYUK PARK <hyeok831@gmail.com>
2024-06-13 13:44:24 +0900

    Title: 두 선분, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240613/두 선분/two-lines.py
+++ 240613/두 선분/two-lines.py
@@ -1,8 +1,15 @@
 a, b, c, d = tuple(map(int, input().split()))
-check = [0] * 101
+# check = [0] * 101
 
+def intersecting(a, b, c, d):
+    # 겹치지 않는 경우에 대한 처리를 먼저 진행합니다. 
+    if b < c or d < a:
+        return False
+    # 나머지는 전부 겹치는 경우라고 볼 수 있습니다.
+    else:
+        return True 
 
-if b < c or d < a:
+if intersecting(a, b, c, d):
     print("nonintersecting")
 else:
     print("intersecting")
