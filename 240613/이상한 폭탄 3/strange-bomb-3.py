63711dd07522b96e84f0a06b856dea321fe5d170
JAEHYUK PARK <hyeok831@gmail.com>
2024-06-13 10:39:30 +0900

    Title: 이상한 폭탄 3, Time: 139ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240613/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,44 @@
+MAX_A = 1000000
+
+# 변수 선언 및 입력 
+n, k = tuple(map(int, input().split()))
+num = [
+    int(input())
+    for _ in range(n)
+]
+
+bomb = [0] * (MAX_A + 1)
+explode = [False] * n 
+
+maxval = 1
+maxidx = 0
+
+# 모든 쌍에 대해서 터질 수 있는 폭탄을 찾고
+# 터진 폭탄의 개수를 저장합니다.
+for i in range(n):
+    for j in range(i + 1, n):
+        # 거리가 k를 초과하는 경우 넘어갑니다. 
+        if j - i > k:
+            break 
+
+        # 두 폭탄의 번호가 다를 경우 터지지 않습니다.
+        if num[i] != num[j]:
+            continue
+
+        # 두 폭탄의 번호가 같을 경우 폭탄은 터집니다.
+        # 해당 폭탄이 이미 터진 폭탄인지 확인하고,
+        # 아직 터지지 않은 폭탄이라면 터진 폭탄의 개수를 갱신합니다. 
+        if explode[i] == False:
+            bomb[num[i]] += 1;
+            explode[i] = True
+
+        if explode[j] == False:
+            bomb[num[j]] += 1;
+            explode[j] = True
+
+for i in range(MAX_A + 1):
+    if maxval <=bomb[i]:
+        maxval = bomb[i]
+        maxidx = i
+
+print(maxidx)
