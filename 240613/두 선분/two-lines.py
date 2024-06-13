9b451b53ab0f0e0fe1007be7e0d0f1e3a885eaff
JAEHYUK PARK <hyeok831@gmail.com>
2024-06-13 13:41:46 +0900

    Title: 두 선분, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240613/두 선분/two-lines.py
+++ 240613/두 선분/two-lines.py
@@ -1,19 +1,26 @@
 a, b, c, d = tuple(map(int, input().split()))
 check = [0] * 101
 
-for i in range(a, b + 1):
-    check[i] += 1
 
-for j in range(c, d + 1):
-    check[j] += 1
-
-ans = False
-
-for finding in check:    
-    if finding >= 2:
-        ans = True
-
-if ans:
-    print("intersecting")
-else:
+if b < c or d < a:
     print("nonintersecting")
+else:
+    print("intersecting")
+
+
+# for i in range(a, b + 1):
+#     check[i] += 1
+
+# for j in range(c, d + 1):
+#     check[j] += 1
+
+# ans = False
+
+# for finding in check:    
+#     if finding >= 2:
+#         ans = True
+
+# if ans:
+#     print("intersecting")
+# else:
+#     print("nonintersecting")
