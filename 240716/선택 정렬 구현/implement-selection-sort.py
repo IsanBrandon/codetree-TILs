ccf399eeffc9646eacfb2ed17ad51ac8f2748e88
JAEHYUK PARK <hyeok831@gmail.com>
2024-07-16 15:20:58 +0900

    Title: 선택 정렬 구현, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/선택 정렬 구현/implement-selection-sort.py
+++ 240716/선택 정렬 구현/implement-selection-sort.py
@@ -2,22 +2,23 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 
-def seletion_sort(arr):
-    size = len(arr)
+def seletion_sort():
+    # size = len(arr)
 
-    for i in range(size - 1):
+    for i in range(n - 1):
         minimum = i
-        for j in range(i+1, size):
+        for j in range(i+1, n):
             if arr[j] < arr[minimum]:
                 minimum = j 
-        tmp = arr[i]
-        arr[i] = arr[minimum]
-        arr[minimum] = tmp
+        # tmp = arr[i]
+        # arr[i] = arr[minimum]
+        # arr[minimum] = tmp
+        arr[i], arr[minimum] = arr[minimum], arr[i]
     
     return arr
 
 
-seletion_sort(arr)
+seletion_sort()
 
 for elem in arr:
     print(elem, end=" ")
