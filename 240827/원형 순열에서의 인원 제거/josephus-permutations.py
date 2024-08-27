72e81334fee2d440689a2131fad1091cbb5a3e11
JAEHYUK PARK <hyeok831@gmail.com>
2024-08-27 16:59:02 +0900

    Title: 원형 순열에서의 인원 제거, Time: 402ms, Memory: 31MB, Status: Passed - CodeTree

--- 240827/원형 순열에서의 인원 제거/josephus-permutations.py
+++ 240827/원형 순열에서의 인원 제거/josephus-permutations.py
@@ -1,28 +1,28 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):         # 빈 큐 하나를 생성합니다.
+    def __init__(self):          # 빈 큐 하나를 생성합니다.
         self.dq = deque()
-
-    def push(self, item):       # 큐의 맨 뒤에 데이터를 추가합니다. 
+                
+    def push(self, item):        # 큐의 맨 뒤에 데이터를 추가합니다.
         self.dq.append(item)
-
-    def empty(self):            # 큐가 비어있으면 True를 반환합니다.
+                
+    def empty(self):             # 큐가 비어있으면 True를 반환합니다.
         return not self.dq
-
-    def size(self):             # 큐에 들어있는 데이터 수를 반환합니다.
+                
+    def size(self):              # 큐에 들어있는 데이터 수를 반환합니다.
         return len(self.dq)
-
-    def pop(self):              # 큐의 맨 앞에 있는 데이터를 반환하고 제거합니다.
+        
+    def pop(self):               # 큐의 맨 앞에 있는 데이터를 반환하고 제거합니다.
         if self.empty():
             raise Exception("Queue is empty")
-
+            
         return self.dq.popleft()
-
-    def front(self):            # 큐의 맨 앞에 있는 데이터를 제거하지 않고 반환합니다.
+                
+    def front(self):             # 큐의 맨 앞에 있는 데이터를 제거하지 않고 반환합니다.
         if self.empty():
-            rais Exception("Queue is empty")
-
+            raise Exception("Queue is empty")
+                        
         return self.dq[0]
 
 # 변수 선언 및 입력:
@@ -30,7 +30,7 @@ n, k = tuple(map(int, input().split()))
 q = Queue()
 
 # 큐로 현재 남은 사람들을 관리합니다.
-for i in range(1, n+1):
+for i in range(1, n + 1):
     q.push(i)
 
 while q.size() > 1:
