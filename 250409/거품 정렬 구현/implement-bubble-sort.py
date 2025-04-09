fcc7698ae3223d2a8c2f091b625fde122d5ab272
JAEHYUK PARK <hyeok831@gmail.com>
2025-04-09 14:03:59 +0900

    Title: 거품 정렬 구현, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250409/거품 정렬 구현/implement-bubble-sort.py
+++ 250409/거품 정렬 구현/implement-bubble-sort.py
@@ -7,7 +7,7 @@ def bubble_sort():
     while not is_sorted:
         is_sorted = True
         for i in range(n - 1):
-            if arr[i] > arr[i + 1]
+            if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 is_sorted = False
 
