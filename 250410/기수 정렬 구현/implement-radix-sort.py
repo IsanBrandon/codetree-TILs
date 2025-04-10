c7e84fd48334647d7e4185f10cc58f3eae061ce6
JAEHYUK PARK <hyeok831@gmail.com>
2025-04-10 18:39:06 +0900

    Title: 기수 정렬 구현, Time: 93ms, Memory: 26MB, Status: Passed - Codetree

---
+++ 250410/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,28 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+MAX_K = 6
+MAX_DIGIT = 10
+
+def radix_sort():
+    global arr
+
+    p = 1
+    for pos in range(MAX_K):
+        arr_new = [[] for _ in range(MAX_DIGIT)]
+        for elem in arr:
+            digit = (elem // p) % 10
+            arr_new[digit].append(elem)
+
+        arr = []
+        for digit in range(MAX_DIGIT):
+            for elem in arr_new[digit]:
+                arr.append(elem)
+
+        p *= 10 
+
+radix_sort()
+
+for elem in arr:
+    print(elem, end=" ")
